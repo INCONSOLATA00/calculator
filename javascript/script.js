bdb4ba067e69754ecc0a9154de19bb0a63c4ae03
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-03-17 18:20:20 -0400

    added initial logic for @keyboard / screen_buttons

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,35 @@
-
+let  alsoInput;
 
 const input_field = document.querySelector("div > input");
 input_field.value = "value";
 
 const operands = document.querySelectorAll("#numpad > button");
 
+
+window.addEventListener("keyup", keyboard_input);
+
 for(let i = 0; i < operands.length -1; i++) {
 operands[i].addEventListener('click', operand_click);
 }
 
-function  operand_click(e) {
-console.log(e.target.textContent);
-input_field.value += e.target.textContent; // may need to store values, may need conditional for "."
+
+
+function keyboard_input(e) {
+console.log(alsoInput)
+// only if it is a number key that is engaged (conditional)
+
+alsoInput += e.key
+input_field.value = alsoInput;
 }
+
+function  operand_click(e) {
+console.log(alsoInput)
+
+if(e.target.textContent == ".")  { // change from literal value to stored to account for text inputs
+operands[11].removeEventListener("#numpad > button");
+}
+
+alsoInput += e.target.textContent;
+input_field.value += alsoInput;
+}
+
