b35a4a9c24c2ab5cf324897c6956db9d033731d1
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-03-17 16:47:38 -0400

    added initial logic, append text

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,15 @@
-console.log('hello');
+
+
+const input_field = document.querySelector("div > input");
+input_field.value = "value";
+
+const operands = document.querySelectorAll("#numpad > button");
+
+for(let i = 0; i < operands.length -1; i++) {
+operands[i].addEventListener('click', operand_click);
+}
+
+function  operand_click(e) {
+console.log(e.target.textContent);
+input_field.value += e.target.textContent; // may need to store values, may need conditional for "."
+}
