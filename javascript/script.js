37b63cc0c3d077cd0b189cb19e294427f94eeafd
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-03-29 00:02:07 -0400

    *

--- javascript/script.js
+++ javascript/script.js
@@ -23,9 +23,10 @@ for(let i = 0; i < arithmetic_functions.length; i++){
 arithmetic_functions[i].addEventListener('click', arithmetic);
 }
 
-function arithmetic(e) {
+function arithmetic(e) { // operators only*
+
 actual_previousValue = previousValue;
-previousValue = e.target.textContent + ""; // SEE HERE, ONLY PASSES OPERATORS, NOT NUMBERS
+previousValue = e.target.textContent + "";
 
 if(alsoInput > 0) {
 isPair = true;
@@ -65,16 +66,16 @@ input_field.value = alsoInput;
 
 
 const functions_equals = document.querySelector(".assignment03 > button").addEventListener('click', (e) => {
-console.log('equals')
 
 if(previousValue !== actual_previousValue) { console.log('reached01')
 inputSum = +inputSum + +alsoInput;
 also_alsoInput = +alsoInput;
+console.log('equals')
 
 input_field.value = inputSum;
 alsoInput = "";
 
-} else if(previousValue == actual_previousValue) { console.log('reached02') // the last value being assigned is not a number* && // does not contain numerical values; check push statement*
+} else if(previousValue == actual_previousValue) { console.log('reached02')
 inputSum += +currentValue[currentValue.length -1]
 input_field.value =  inputSum;
 }
@@ -99,19 +100,18 @@ if(isNaN(e.key) == true) {
 actual_previousValue = previousValue;
 previousValue = e.key + "";
 
-input_field.value = "";
-inputSum += e.key
+inputSum += e.key;
 input_field.value = inputSum;
 
-} else { // is not pair
+} else {
 actual_previousValue = previousValue;
 previousValue = e.key + "";
 
-alsoInput += e.key
+alsoInput += e.key;
 input_field.value = alsoInput;}}
 
 
-function operand_click(e) { // when exactly is delete being called?
+function operand_click(e) {
 actual_previousValue = previousValue;
 previousValue = e.target.textContent + "";
 currentValue.push(previousValue);
@@ -130,42 +130,31 @@ input_field.value = alsoInput;
 // someArray.some((opr)=> e.key == opr) == false, allow use of keyboard to enter operators*
 
 function operand_delete(e){
-actual_previousValue = previousValue;
-
-if(isKeyboard == false){ console.log('isMouse')
-previousValue = e.target.textContent + ""
-} else { console.log('isKeyboard')
-previousValue = e.key + ""};
 console.log('delete')
 
 if((e.key == "Backspace" && isPair == false) || (e.target.textContent == "DEL" && isPair == false)) { console.log('reached03')
-alsoInput = alsoInput + "";
 
-if(isPair == false) { console.log('reached final') // SEE HERE //  && alsoInput > 0
-
-alsoInput = alsoInput.slice(0, alsoInput.length -1);
-alsoInput = +alsoInput;
-input_field.value = alsoInput;}
-
-if(isPair == false && alsoInput == "0") {
-
-alsoInput = "";
-input_field.value = alsoInput;}}
-
-if((e.key == "Backspace" && isPair == true) || (e.target.textContent == "DEL" && isPair == true)) { console.log('reached04')
 inputSum = inputSum + "";
-
-if(isPair == true) { // SEE HERE // && inputSum.length > 0
-
 inputSum = inputSum.slice(0, inputSum.length -1);
 inputSum = +inputSum;
-input_field.value = inputSum;}
-
-if(isPair == true && inputSum == "0") {
-
-inputSum = "";
 input_field.value = inputSum;
-}}}
+}
+
+if((e.key == "Backspace" && isPair == true) || (e.target.textContent == "DEL" && isPair == true)) { console.log('reached04')
+
+inputSum = inputSum + "";
+inputSum = inputSum.slice(0, inputSum.length -1);
+inputSum = +inputSum;
+input_field.value = inputSum;
+}
+
+if(alsoInput.length == 1 || inputSum.length == 1) {
+inputSum = inputSum + "";
+inputSum = inputSum.slice(0, inputSum.length -1);
+alsoInput = "";
+input_field.value = alsoInput;
+}}
+
 
 
 function determine_arithmetic(value){
